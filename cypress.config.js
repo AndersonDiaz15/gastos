fec2eb9bdf2a53ce2553bc5424cd190edc1d0b2b
AndersonDiaz15 <anjodivi15@gmail.com>
2025-09-26 09:40:56 -0500

    Agregar workflow CI/CD

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://paginas-pruebas.lovestoblog.com/",
+    baseUrl: "http://paginas-pruebas.lovestoblog.com/",
     setupNodeEvents(on, config) {
       // Eventos si necesitas en un futuro
     },
