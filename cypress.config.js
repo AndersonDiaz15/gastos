907058faf3c3a3947aa8ad5e57a64eb4ddd15678
AndersonDiaz15 <anjodivi15@gmail.com>
2025-10-07 11:59:36 -0500

    probando reporte

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "cypress";
 import allureWriter from "@shelex/cypress-allure-plugin/writer.js";
 
 module.exports = defineConfig({
+  reporter: "mocha-allure-reporter",
+  reporterOptions: {
+    resultsDir: "allure-results", // carpeta donde se guardan los resultados
+  },
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
